d63fa630ec1baccf38739445085f3daec8b3cf2a
janirefdez <janire_fernandez94@hotmail.com>
2022-02-19 22:39:25 +0100

    Initial Commit

---
+++ BackpackShopApp/BackpackShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackpackShopAppApp.swift
+//  BackpackShopApp
+//
+//  Created by Janire Fernandez on 19/2/22.
+//
+
+import SwiftUI
+
+@main
+struct BackpackShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
